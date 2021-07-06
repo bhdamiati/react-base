59cd13b35446bdc88e06722143d8793e96ca3c65
Bruno Damiati <bhdamiati@gmail.com>
2021-07-06 20:36:06 -0300

    [master] - Creation of base repository for reactJS projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Router } from 'react-router-dom'
 
-function App() {
+import Routes from './routes'
+import history from './services/history'
+
+import GlobalStyles from './styles/globalStyles'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router history={history}>
+      <Routes/>
+      <GlobalStyles/>
+    </Router>
+  )
 }
 
-export default App;
+export default App
